73a6c27ece8e1d8f1f25f9df959e73d4c00ab232
David Pierce <david.dean.pierce@gmail.com>
2018-05-27 16:33:17 -0700

    Update library code to ES6 and more disciplined JS
    We learned a lot in 4 years...

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,54 @@
-var _            = require('underscore');
-var gulp         = require('gulp');
-var runSequence  = require('run-sequence');
-var awspublish   = require('gulp-awspublish');
-var sass         = require('gulp-sass');
-var transblogify = require('./lib/transblogify');
-var del          = require('del');
-var mkdirp       = require('mkdirp');
-var path         = require('path');
+const _            = require('underscore');
+const awspublish   = require('gulp-awspublish');
+const del          = require('del');
+const gulp         = require('gulp');
+const mkdirp       = require('mkdirp');
+const path         = require('path');
+const runSequence  = require('run-sequence');
+const sass         = require('gulp-sass');
+const transblogify = require('./lib/transblogify');
 
 // Live Reload
 // http://rhumaric.com/2014/01/livereload-magic-gulp-style/
-var express = require('express');
-var app;
+const express = require('express');
+let app;
 
-var EXPRESS_PORT = 4000;
-var EXPRESS_ROOT = __dirname + '/build';
-var LIVERELOAD_PORT = 35729;
+const EXPRESS_PORT = 4000;
+const EXPRESS_ROOT = __dirname + '/build';
+const LIVERELOAD_PORT = 35729;
 
-var startExpress = function () {
+function startExpress() {
   if (!app) {
     app = express();
     app.use(require('connect-livereload')());
     app.use(express.static(EXPRESS_ROOT));
     app.listen(EXPRESS_PORT);
   }
-};
+}
 
-var lr;
-var startLiveReload = function () {
+let lr = null;
+function startLiveReload() {
   lr = require('tiny-lr')();
   lr.listen(LIVERELOAD_PORT);
-};
+}
 
-var notifyLivereload = function (event) {
+function notifyLivereload(event) {
   if (event) {
     console.log(event);
     gulp.src(event.path, {read: false})
       .pipe(require('gulp-livereload')(lr));
   }
   //var fileName = require('path').relative(EXPRESS_ROOT, event.path);
-
   //lr.changed({ body: { files: [fileName] } });
-};
+}
 
 // Generate pygments stylesheets:
 // http://funcptr.net/2011/11/27/generating-stylesheets-for-pygments/
 // https://github.com/chjj/marked
 
-
 gulp.task('clean', del.bind(del, [ 'build/**/*']));
 
-gulp.task('posts', function () {
+gulp.task('posts', () => {
   return transblogify.posts({
     'postsDir': 'posts',
     'postsPattern': '*.md',
@@ -61,7 +59,7 @@ gulp.task('posts', function () {
   });
 });
 
-gulp.task('pages', function () {
+gulp.task('pages', () => {
   return transblogify.pages({
     'templatesDir': 'templates',
     'pagesDir': 'pages',
@@ -69,7 +67,7 @@ gulp.task('pages', function () {
   });
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   mkdirp.sync('./build/css');
 
   return gulp.src('./assets/sass/application.scss')
@@ -80,12 +78,20 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('watchsrc', function () {
-  gulp.watch(['assets/sass/*/**.scss'], ['sass']);
-  gulp.watch(['posts/**/*', 'pages/**/*', 'templates/**/*' ,'data/**/*'], ['build']);
+gulp.task('watchsrc', () => {
+  gulp.watch([ 'assets/sass/*/**.scss' ], [ 'sass' ]);
+  gulp.watch(
+    [
+      'posts/**/*',
+      'pages/**/*',
+      'templates/**/*',
+      'data/**/*'
+    ],
+    ['build']
+  );
 });
 
-gulp.task('move-assets', function (cb) {
+gulp.task('move-assets', () => {
   mkdirp.sync('build/images');
   mkdirp.sync('build/js');
 
@@ -96,25 +102,25 @@ gulp.task('move-assets', function (cb) {
     .pipe(gulp.dest('build/images'));
 });
 
-gulp.task('build', function () {
+gulp.task('build', () => {
   runSequence('clean', 'pages', 'posts', 'sass', 'move-assets');
 });
 
-gulp.task('autoserve', ['build', 'watchsrc'], function () {
+gulp.task('autoserve', ['build', 'watchsrc'], () => {
   startExpress();
   startLiveReload();
 
   // Wait 2 seconds before watching for live reload
   setTimeout(function () {
     gulp.watch(['build/**/*'], function (changeEvent) {
-      var changedFile = path.relative(EXPRESS_ROOT, changeEvent.path);
+      let changedFile = path.relative(EXPRESS_ROOT, changeEvent.path);
       lr.changed({ body: { files: [changedFile] } });
     });
   }, 2000);
 });
 
-gulp.task('publish', function () {
-  var publisher = awspublish.create(require('./awscredentials.json'));
+gulp.task('publish', () => {
+  const publisher = awspublish.create(require('./awscredentials.json'));
 
   return gulp.src('build/**/*')
     .pipe(publisher.publish())
