c52b4fca301455fb55f8bb2d305ddceaa0c68a03
David Pierce <david.dean.pierce@gmail.com>
2021-02-08 17:29:58 -0800

    Link image attribution

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -103,7 +103,15 @@ const IndexPage = ({ location }) => {
       </div>
       <div className={styles.profileContainer}>
         <p>
-          Thanks to Sound Roots Photography, to whom the credit belongs for my profile photo.
+          Thanks to{' '}
+          <a
+            href='https://www.soundrootsphotography.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Sound Roots Photography
+          </a>
+          , to whom the credit belongs for my profile photo.
         </p>
       </div>
     </HomeLayout>
